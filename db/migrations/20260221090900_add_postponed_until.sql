468fc7fb57443092394d271b94c68874cbc4c90d
Andres Robam <andresrobam@gmail.com>
2026-02-22 00:14:19 +0200

    Fix migration

--- db/migrations/20260221090900_add_postponed_until.sql
+++ db/migrations/20260221090900_add_postponed_until.sql
@@ -1,10 +1,7 @@
--- Add postponed_until column to task_status table
--- This allows tasks to be postponed until a specific time
--- The main loop will filter out postponed tasks until their time arrives
-
+-- migrate:up
 ALTER TABLE task_status ADD COLUMN postponed_until timestamptz NULL;
-
 GRANT UPDATE (postponed_until) ON TABLE task_status TO agent;
 
--- Create index for efficient filtering of postponed tasks
 CREATE INDEX idx_task_status_postponed_until ON task_status(postponed_until) WHERE postponed_until IS NOT NULL;
+-- migrate:down
+ALTER TABLE task_status DROP COLUMN postponed_until
